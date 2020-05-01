16e38e37a9444d9401391d7480a5fe3425828232
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-01 16:34:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
 def greet_characters(array)
+  
+  array.each do |dwarf|
+    puts "Hello" #{dwarf} !
+  end 
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
